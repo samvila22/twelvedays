a0367c69443c490460926a7386101bd382cbdb0d
Sam Vila <poonslayer69@Sams-MacBook-Air-3.local>
2021-05-14 15:02:56 -0700

    added plrualize

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,26 @@ library(testthat)
 library(twelvedays)
 
 test_check("twelvedays")
+
+test_check("pluralize_gift")
+
+context("changing all input strings into plural")
+
+test_that("running inputs that arent strings", {
+
+  expect_error( pluralize_gift(1) )
+
+})
+#returns "please enter a character object
+
+test_that(inputting_character, {
+
+  x <- "samuel"
+  expect_equal( returns: "samuels" )
+
+  x <- "party"
+  expect_equal( returns : "parties")
+
+})
+
+

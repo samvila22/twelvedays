77f83bc9a396d0a772abad4b67982206b22a5416
Sam Vila <poonslayer69@Sams-MacBook-Air-3.local>
2021-05-16 07:53:25 -0700

    added make phrase

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,7 +7,7 @@ test_check("pluralize_gift")
 
 context("changing all input strings into plural")
 
-test_that("running inputs that arent strings", {
+test_that("running inputs that arent characters", {
 
   expect_error( pluralize_gift(1) )
 
@@ -24,4 +24,32 @@ test_that(inputting_character, {
 
 })
 
+test_check("make_phrase")
 
+context("making a phrase from all inputs")
+
+test_that("running inputs that arent characters", {
+
+  expect_error( make_phrase("please", "don't", 1) )
+
+})
+#returns "please enter character objects"
+
+test_that(inputting_characters, {
+
+  num <- 4
+  num_word <- "four"
+  item <- "birds"
+  verb <- "meandering"
+  adjective <- "laterally"
+  location <- "to the moon"
+  expect_equal( returns : "four birds meandering laterally to the moon")
+
+  num <- 6
+  num_word <- "sixth"
+  item <- "flappers"
+  verb <- "flapping"
+  adjective <- "independently"
+  location <- "at the assistant"
+  expect_equal( returns : "six flappers flapping indepedently at the assistant")
+})
